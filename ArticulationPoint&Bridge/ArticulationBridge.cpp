81b8cd1e28ff06f0020653a724b0a93cc568a419
Zakaria <zakaria43@student.sust.edu>
2022-03-31 12:33:29 +0600

    articulationPoint&Bridge

--- ArticulationPoint&Bridge/ArticulationBridge.cpp
+++ ArticulationPoint&Bridge/ArticulationBridge.cpp
@@ -1,5 +1,88 @@
 /*
 * @Author: Zakaria 
 * @Date:   2022-03-30 17:48:29
-* @Last Modified time: 2022-03-30 17:48:29
+* @Last Modified time: 2022-03-31 12:26:11
 */
+#include<bits/stdc++.h>
+#define pb       push_back
+#define pi       acos(-1.0)
+#define mod      1000000007
+#define test     int t; cin>>t ;while(t--)
+typedef long long ll;
+using namespace std;                                                           
+const ll inf=LLONG_MAX;
+
+vector<int>node[10003];
+
+int lowtime[10003],intime[10003],vis[10003];
+
+vector<pair<int,int>>edge;
+
+int timer;
+
+void dfs(int p, int parent)
+{   
+     
+       intime[p]=lowtime[p]=timer;
+       timer++;
+       
+       vis[p]=1;
+
+       for(int child:node[p])
+       {
+           
+           if(child==parent)
+           continue;
+
+           if(vis[child])
+           {
+              lowtime[p]=min(lowtime[p], intime[child]); // node - child is a back edge
+           }
+
+           else
+           {
+               // node -  child is a forward edge 
+               dfs(child,p);
+
+               if(lowtime[child] > intime[p])
+                edge.push_back({p , child});
+              
+               lowtime[p] = min(lowtime[p] , lowtime[child]);
+
+           }
+
+           
+
+       }
+
+}
+
+
+
+int main()
+{
+      
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+     
+     
+
+     int n,m;
+     cin>>n>>m;
+     while(m--)
+     {
+        int p,q;
+        cin>>p>>q;
+        node[p].push_back(q);
+        node[q].push_back(p);
+     }
+
+     dfs(1,-1);
+     
+     for(auto i:edge)
+      cout<<i.first<<" to "<<i.second<<" is a birdge"<<endl;
+     
+      
+     return 0;
+       
+}
--- ArticulationPoint&Bridge/ArticulationPoint.cpp
+++ ArticulationPoint&Bridge/ArticulationPoint.cpp
@@ -1,5 +1,93 @@
 /*
 * @Author: Zakaria 
 * @Date:   2022-03-30 17:48:41
-* @Last Modified time: 2022-03-30 17:48:41
+* @Last Modified time: 2022-03-31 12:29:01
 */
+#include<bits/stdc++.h>
+#define pb       push_back
+#define pi       acos(-1.0)
+#define mod      1000000007
+#define test     int t; cin>>t ;while(t--)
+typedef long long ll;
+using namespace std;                                                           
+const ll inf=LLONG_MAX;
+
+vector<int>node[10003];
+
+int lowtime[10003],intime[10003],vis[10003];
+
+set<int>cut_vertex;
+
+int timer;
+
+void IS_CUTPOINT(int x)
+{
+    cut_vertex.insert(x);
+}
+
+void dfs(int p, int parent)
+{   
+     
+       intime[p]=lowtime[p]=timer;
+       timer++;
+       int children=0;
+       vis[p]=1;
+
+       for(int child:node[p])
+       {
+           
+           if(child==parent)
+           continue;
+
+           if(vis[child])
+           {
+              lowtime[p]=min(lowtime[p], intime[child]); // node - child is a back edge
+           }
+
+           else
+           {
+               // node -  child is a forward edge 
+               dfs(child,p);
+
+               if(lowtime[child] >= intime[p] && parent!=-1)
+                IS_CUTPOINT(p);
+
+               lowtime[p] = min(lowtime[p] , lowtime[child]);
+               children++;
+           }
+
+       }
+
+       if(parent == -1 && children>1)//for root
+          IS_CUTPOINT(p);
+}
+
+
+
+int main()
+{
+      
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+     
+     
+
+     int n,m;
+     cin>>n>>m;
+     while(m--)
+     {
+        int p,q;
+        cin>>p>>q;
+        node[p].push_back(q);
+        node[q].push_back(p);
+     }
+
+     dfs(1,-1);
+     
+     for(auto i:cut_vertex)
+     cout<< i <<" is a cut vertex"<<endl;
+     
+      
+     return 0;
+       
+}
