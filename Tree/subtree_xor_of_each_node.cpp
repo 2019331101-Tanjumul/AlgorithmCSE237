9be30dedf926fe7fbc31fa75243f9df72e6e0429
Zakaria Hossain <68740480+2019331043-Zakaria@users.noreply.github.com>
2022-04-25 13:55:03 +0600

    Update subtree_xor_of_each_node.cpp

--- Tree/subtree_xor_of_each_node.cpp
+++ Tree/subtree_xor_of_each_node.cpp
@@ -1,5 +1,22 @@
 /*
 * @Author: Zakaria 
 * @Date:   2022-04-25 00:56:21
-* @Last Modified time: 2022-04-25 00:56:21
+* @Last Modified time: 2022-04-25 01:13:38
 */
+ll dfs(int p)
+{
+     vis[p]=1;
+     dis[p]=a[p];
+     ll cur=a[p];
+     for(auto i:node[p])
+     {
+         if(vis[i]==0)
+         {
+             cur^=dfs(i);
+ 
+         }
+     }
+     dis[p]=cur;
+     return cur;
+ 
+}
