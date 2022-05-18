c153e3397ef48de04d8478fcd5e94545849ec8e5
Zakaria <zakaria43@student.sust.edu>
2022-05-18 23:22:01 +0600

    Backtracking

--- Backtracking/Backtracking.cpp
+++ Backtracking/Backtracking.cpp
@@ -1,5 +1,70 @@
 /*
 * @Author: Zakaria 
-* @Date:   2022-03-30 17:47:15
-* @Last Modified time: 2022-03-30 17:47:15
+* @Date:   2022-04-01 01:08:32
+* @Last Modified time: 2022-05-18 23:21:23
 */
+#include <bits/stdc++.h>
+#define  pb          push_back
+#define  f           first
+#define  s           second
+#define  pi          acos(-1.0)
+#define  LCM(a,b)    a*b/__gcd(a,b)
+#define  GCD(a,b)    __gcd(a,b)
+#define  sof         sizeof
+#define  endl        '\n'
+#define  MAX         100002
+typedef long long ll;
+using namespace std;
+
+int n,m;
+
+vector<int>order;
+int vis[MAX];
+
+void Backtrack()
+{
+        if(order.size()==n)
+        {
+            for(auto i:order)
+               cout<<i<<" ";
+            cout<<endl;
+            return ;
+        }
+
+        for(int i=1 ; i<=n ; i++)
+        {
+              if(vis[i])
+               continue;
+
+              order.push_back(i);
+              vis[i]=1;
+              Backtrack();
+              order.pop_back();
+              vis[i]=0;
+        }
+}
+void Solve()
+{  
+         
+         cin>>n>>m;
+         Backtrack();
+
+}
+
+int main()
+{
+         
+          ios::sync_with_stdio(false);
+          cin.tie(0);
+           
+          int tt=1;
+        //  cin>>tt;
+          while(tt--)
+          {
+             Solve();
+          }
+
+
+          return 0;
+        
+}
