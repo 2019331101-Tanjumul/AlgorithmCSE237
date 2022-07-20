1a63812b78348a37c29a70432e56979c35ee835e
Zakaria Hossain <68740480+2019331043-Zakaria@users.noreply.github.com>
2022-07-21 00:04:50 +0600

    Update PrimsAlgortithm.cpp

--- MST/PrimsAlgortithm.cpp
+++ MST/PrimsAlgortithm.cpp
@@ -1,5 +1,83 @@
 /*
 * @Author: Zakaria 
-* @Date:   2022-03-30 17:50:40
-* @Last Modified time: 2022-03-30 17:50:40
+* @Date:   2022-04-01 01:08:32
+* @Last Modified time: 2022-07-21 00:03:09
 */
+#include <bits/stdc++.h>
+#define  pb          push_back
+#define  f           first
+#define  s           second
+#define  pi          acos(-1.0)
+#define  LCM(a,b)    a*b/__gcd(a,b)
+#define  GCD(a,b)    __gcd(a,b)
+#define  sof         sizeof
+#define  endl        '\n'
+#define  MAX         500002
+typedef long long ll;
+using namespace std;
+
+
+int n, m;
+vector<pair<ll, ll> > v[MAX];
+int take[MAX];
+void Solve()
+{
+         
+         cin>>n>>m;
+       
+         for(int i=0 ; i<m ; i++)
+         { 
+             int x, y, w;
+             cin>>x>>y>>w;
+             v[x].push_back({w, y});
+             v[y].push_back({w, x});
+         }
+
+         priority_queue<pair<ll, ll>, vector<pair<ll,ll > >, greater<pair<ll, ll > > > pq;
+         ll ans=0;
+         pq.push({0, 1});
+
+         while(pq.size())
+         {
+                
+              pair<ll,ll>p=pq.top();
+              pq.pop();
+
+              if(take[p.s])
+               continue;
+
+              ans+=p.f;
+              take[p.s]=1;
+
+              for(auto i:v[p.s])
+              {
+                  if(!take[i.s])
+                     pq.push({i});
+              }
+
+         }
+
+         cout<<ans<<endl;
+
+
+
+
+         
+}
+
+int main()
+{
+         
+          ios::sync_with_stdio(false);
+          cin.tie(0);
+           
+          int tt=1;
+       //   cin>>tt;
+          while(tt--)
+          {
+             Solve();
+          }
+
+          return 0;
+        
+}
