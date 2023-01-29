896375b09e303c44613400583229e7084a49ba4c
2019331101-Tanjumul <crackerroot4@gmail.com>
2023-01-30 02:30:35 +0600

    addin an array of vectors

---
+++ Vector/vector.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+void printVector(vetor<int> v){ 
+    cout<<"Size is :"<<v.size()<<endl;
+    for(int i =0 ; i<v.size();i++){ 
+        cout<<v[i]<<" "; 
+    }
+}
+
+int main(){ 
+int N; 
+cin>>N;     
+vector<int> v[N]; 
+for(int i=0; i<N; i++){ 
+    int n; 
+    cin>>n; 
+    for(int j=0 ; j<n; i++){ 
+        int x; 
+        cin>>x; 
+        v[i].push_back(x); 
+
+    }
+}
+  for(int i=0; i<N; i++){ 
+      printVector(v[i]); 
+
+  }
+
+
+}
